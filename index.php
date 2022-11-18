6ce3a7d22804a8e3ea5e0e4345c6aabe0efa2200
Stef Lipka <steflipka021@gmail.com>
2022-11-18 09:19:47 +0100

    index update met user php erbij

--- index.php
+++ index.php
@@ -1,16 +1,21 @@
 <?php
 
+// Importeer de DB en de verschillende tabel scripts:
 require_once("lib/database.php");
 require_once("lib/artikel.php");
+require_once("lib/user.php");
 
 /// INIT
 $db = new database();
 $art = new artikel($db->getConnection());
+$usr = new user($db->getConnection());
 
 
 /// VERWERK 
 $data = $art->selecteerArtikel(2);
+$dataUser = $usr->selecteerUser(3);
 
 /// RETURN
 echo "<pre>";
 var_dump($data);
+var_dump($dataUser);
