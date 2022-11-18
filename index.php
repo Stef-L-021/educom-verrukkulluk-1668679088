97b77d6d08390deee58f63dde8c6c14a09d2442e
Stef Lipka <steflipka021@gmail.com>
2022-11-18 09:40:36 +0100

    index update met keuken_type

--- index.php
+++ index.php
@@ -4,18 +4,22 @@
 require_once("lib/database.php");
 require_once("lib/artikel.php");
 require_once("lib/user.php");
+require_once("lib/keuken_type.php");
 
-/// INIT
+/// INIT    link de classes aan variables
 $db = new database();
 $art = new artikel($db->getConnection());
 $usr = new user($db->getConnection());
+$keuken = new keuken_type($db->getConnection());
 
 
-/// VERWERK 
+/// VERWERK     de varialbe + select functie in een nieuwe variable
 $data = $art->selecteerArtikel(2);
 $dataUser = $usr->selecteerUser(3);
+$datakeuken = $keuken->selecteerKeuken_type(4);
 
-/// RETURN
+/// RETURN      de nieuwe var
 echo "<pre>";
 var_dump($data);
 var_dump($dataUser);
+var_dump($datakeuken);
