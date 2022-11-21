1d46bf8e485e4ded5a415726c57498e8b8ee5705
Stef Lipka <steflipka021@gmail.com>
2022-11-21 18:06:26 +0100

    ingredient en index update

--- index.php
+++ index.php
@@ -14,14 +14,14 @@ $art = new artikel($db->getConnection());
 $usr = new user($db->getConnection());
 $keuken = new keuken_type($db->getConnection());
 $gerecht_info = new gerecht_info($db->getConnection());
-$ingredient = new ingredient($db->getConnection());
+$ingr = new ingredient($db->getConnection());
 
 /// VERWERK     de variable + select functie in een nieuwe variable
 $data = $art->selecteerArtikel(2);
 $dataUser = $usr->selecteerUser(3);
 $datakeuken = $keuken->selecteerKeuken_type(4);
 $data_gerecht_info_user = $gerecht_info->selecteerUserGerecht_info(8);
-$dataIngredient = $ingredient->selecteerIngredient(2);
+$dataIngredient = $ingr->selecteerIngredient(2);                      // test bij gerecht 2
 
 // Add en delete favoriten:
 //$addFavorite = $gerecht_info->addFavorite(2,3);               // Voeg tussen haakjes in het GERECHT_ID,USER_ID voor deze TOE TE VOEGEN
@@ -33,9 +33,10 @@ echo "<pre>";
 //var_dump($dataUser);
 //var_dump($datakeuken);
 //var_dump($data_gerecht_info_user);
+
 var_dump($dataIngredient);
 
 // var_dump(om te checken of een bepaald ID van gerecht_info er wel of niet is);
-echo "data gerecht 10: <br>";
-$data_gerecht_info = $gerecht_info->selecteerGerecht_info(10);
-var_dump($data_gerecht_info);
+//echo "data gerecht 10: <br>";
+//$data_gerecht_info = $gerecht_info->selecteerGerecht_info(10);
+//var_dump($data_gerecht_info);
--- lib/ingredient.php
+++ lib/ingredient.php
@@ -2,59 +2,50 @@
 
 class ingredient {
 
-    private $connection;
+    private $connection; 
+    private $art;
 
     public function __construct($connection) {
         $this->connection = $connection;
+        $this->art = new artikel($connection);
     }
 
-    /*
-    // selectie gerecht
-    public function selecteerArtikel_id($gerecht_id){
-        $sql = "select artikel_id from gerecht where id= $gerecht_id";
-        return($sql);
-
-        $result = mysqli_query($this->connection, $sql);
-        $gerecht = mysqli_fetch_array($result, MYSQLI_ASSOC);
-
+    // 
+    private function selectArtikel($art_id) {
+        $data = $this->art->selecteerArtikel($art_id);          // dit moet selecteer zijn omdat het in de artikel.php ook selecteer is
+        return($data);  
     }
-
-
-    // Selectie methode met SQL van ingredient:
-    private function selecteerIngredient($gerecht_id) {                 
-        $sql = "select * from ingredient where id = $ingredient_id";
-
-        $restult = mysqli_query($this->connection, $sql);
-        $ingredient = mysqli_fetch_array($result, MYSQLI_ASSOC);
-
-        return($ingredient);
-    }
-*/
 
     // selectie ingredient
-    public function selecteerIngredient($gerecht_id){
-        $sql = "select * from ingredient where id= $gerecht_id";
+    public function selecteerIngredient($gerecht_id) {
+        $sql = "SELECT * FROM ingredient WHERE gerecht_id= $gerecht_id";
+        $return = [];
 
         $result = mysqli_query($this->connection, $sql);
 
-            while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+            while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
 
-                
-            }
-            return($result);
+                $art_id = $row["artikel_id"];
+                $artikel = $this->selectArtikel($art_id);
 
-        //$ingredient = mysqli_fetch_array($result, MYSQLI_ASSOC);
+                $return[] = [
+
+                    "id" => $row["id"],                         // ingredient_id
+                    "gerecht_id" => $row["gerecht_id"],
+                    "artikel_id" => $row["artikel_id"],
+                    "aantal" => $row["aantal"],
+                    "naam" => $artikel["naam"],
+                    "omschrijving" => $artikel["omschrijving"],
+                    "prijs" => $artikel["prijs"],
+                    "eenheid" => $artikel["eenheid"],
+                    "verpakking" => $artikel["verpakking"]
+                    //"calorien"
+
+                ]; // Einde return             
+            } // Einde while functie
+
+            return($return);
     }
-
-    // select article 
-
-/*
-    private function selecteerIngredient($gerecht_id) {   
-        $ingr = new ingredient($this->connection);
-        $ingredient = $
-    }
-*/
 }
-
 
 ?>
