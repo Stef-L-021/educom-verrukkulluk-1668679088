9d3e6f6232f1a4402589e9db49659d344e1f33d1
Stef Lipka <steflipka021@gmail.com>
2022-11-22 07:49:45 +0100

    comments toegevoegd

--- lib/ingredient.php
+++ lib/ingredient.php
@@ -2,6 +2,7 @@
 
 class ingredient {
 
+    // 2 nieuwe private variablen maken:
     private $connection; 
     private $art;
 
@@ -10,23 +11,23 @@ class ingredient {
         $this->art = new artikel($connection);
     }
 
-    // 
-    private function selectArtikel($art_id) {
-        $data = $this->art->selecteerArtikel($art_id);          // dit moet selecteer zijn omdat het in de artikel.php ook selecteer is
-        return($data);  
+    // functie voor het selecteren van de artikel tabel
+    private function selectArtikel($art_id) {                   // We maken hier een nieuwe tabel
+        $data = $this->art->selecteerArtikel($art_id);          // dit moet selecteer zijn omdat het in de artikel.php ook selecteer is. We gebruiken de data van artikel.php
+        return($data);                                          // aan het einde van de functie wordt de data returned
     }
 
     // selectie ingredient
-    public function selecteerIngredient($gerecht_id) {
-        $sql = "SELECT * FROM ingredient WHERE gerecht_id= $gerecht_id";
-        $return = [];
+    public function selecteerIngredient($gerecht_id) {                      // nieuwe functie opstellen:
+        $sql = "SELECT * FROM ingredient WHERE gerecht_id= $gerecht_id";    // Dit moet gerecht_id zijn omdat we dit ook in willen vullen!!
+        $return = [];                                                       // We returnen een array
 
-        $result = mysqli_query($this->connection, $sql);
+        $result = mysqli_query($this->connection, $sql);                    // we maken een nieuwe variable genaamd $result en hier komt de sql in die geconnenct is via de connection
 
-            while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+            while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {      // als er een array is is er een loop
 
-                $art_id = $row["artikel_id"];
-                $artikel = $this->selectArtikel($art_id);
+                $art_id = $row["artikel_id"];                               // kan dit niet in hieronder?
+                $artikel = $this->selectArtikel($art_id);                   // variable artikel is de selectArtikel functie de we hebben gemaakt met art_id als invoer
 
                 $return[] = [
 
@@ -44,7 +45,7 @@ class ingredient {
                 ]; // Einde return             
             } // Einde while functie
 
-            return($return);
+        return($return);                                                // Aan t einde van de selecteerIngredient functie returnen we de $return variable die we eerder hebben gemaakt
     }
 }
 
