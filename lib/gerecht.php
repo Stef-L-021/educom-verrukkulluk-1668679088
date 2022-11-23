1f43861d83ea426bae9c423813f32ffa18b421cc
Stef Lipka <steflipka021@gmail.com>
2022-11-23 15:56:02 +0100

    Calorieen moet nog naar gerecht gerefereerd worden

--- lib/gerecht.php
+++ lib/gerecht.php
@@ -3,46 +3,89 @@
 class gerecht {
 
     private $connection;
+    private $gerecht;
 
     public function __construct($connection) {
         $this->connection = $connection;
-        $this->usr = new user($connection);
-        $this->ingr = new ingredient($connection);
+        $this->keuken = new keuken_type($connection);
+        $this->ingredienten = new ingredient($connection);
+        $this->user = new user($connection);
+        $this->gerechtInfo = new gerecht_info($connection);
     }
 
-    // Selecteer User
-    public function selectUser($usr_id) {                   // Dit geeft een error als je een private functie probeert te gebruiken
-        $datausr = $this->usr->selecteerUser($usr_id);
-        return($datausr);
+// Opahalen van andere php file functies en 2 calculaties: --------------------------------------
+
+    // select keuken
+    private function selectKeukenType($keuken_id) {
+        $data= $this->keuken->selecteerKeuken_type($keuken_id);
+        return($data);
     }
 
     // selecteer ingredient
-    // We geven hier het gerecht_id opgeven en dat krijgen we arrays met de ingredienten uit
-    public function selectIngredient($gerecht_id){          // het staat rechts in de ASD dus moet dat dan een private function zijn?
-        $dataingr = $this->ingr->selecteerIngredient($gerecht_id);        // Deze functie kunnen we ophalen uit ingredienten
-        return($dataingr);
+    private function selectIngredient($gerecht_id) {        
+        $data = $this->ingredienten->selecteerIngredient($gerecht_id);        // Deze functie kunnen we ophalen uit ingredienten
+        return($data);
     }
 
-    // Calc calories 
+    // Bereken calorieen: 
+    private function berekenCalorieen($gerecht_id) {
+        $ingredienten= $this->ingredienten->selecteerIngredient($gerecht_id);   // Dit moet vanuit gerecht wordt opgeroepen      // Deze functie moeten we gaan ophalen uit gerecht
+        $calorieen = 0;                                                         // Het wordt aan het begin voor de berekening standaard op 0 gezet
+        $aantal = 0;
+        $verpakking = 0;
+        $totaal = 0;
 
-    // Calc prijs
+        foreach($ingredienten as $ingredient) {
+            $calorieen = $ingredient["calorieen"];             // Is hetzelfde als $calorieen = $calorieen + $ingredient["calorieen"];
+            $aantal = $ingredient["aantal"];
+            $verpakking = $ingredient["verpakking"];
 
-    // Selecteer waardering
-    
-    // Selecteer bereidingswijze
+            $berekening = $calorieen*($aantal/$verpakking);
+            $totaal += $berekening;
+        }
 
-    // Selecteer opmerkingen
+        return($totaal);
+    }
 
+       // Selecteer User
+       private function selectUser($user_id) {
+        $data = $this->user->selecteerUser($user_id);
+        return($data);
+    }
 
-    // Select gerecht
+    // Select gerecht -----------------------------------------------------------
     public function selecteerGerecht($gerecht_id) {     // uit gerecht_info
 
         $sql = "SELECT * FROM gerecht WHERE id = $gerecht_id";
+        $return = [];  
 
         $result = mysqli_query($this->connection, $sql);
-        $gerecht = mysqli_fetch_array($result, MYSQLI_ASSOC);
+        while ($gerecht = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
 
-        return($gerecht);
+            $keuken_id = $gerecht["keuken_id"];                 // ..._id = $gerecht[Naam van de kolom in de gerecht tabel]
+            $keuken = $this->selectKeukenType($keuken_id);
+            
+            $type_id = $gerecht["type_id"];
+            $type= $this->selectKeukenType($type_id);
+
+            $ingredienten_id= $gerecht["id"];
+            $ingredienten = $this->selectIngredient("$ingredienten_id");
+
+            $berekenCalorieen_id = $gerecht["id"];
+            $berekenColorieen = $this->berekenCalorieen($berekenCalorieen_id);
+
+
+
+            $return[] = [
+                "keuken" => $keuken,
+                "type" => $type,
+                "ingredienten" => $ingredienten,
+                "calorieen" => $berekenColorieen
+            ];
+        }
+
+
+        return($return);
     }
 
 
