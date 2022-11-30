69fc7e4511f4b1dfc76741a8bd64f3945606d065
Stef Lipka <steflipka021@gmail.com>
2022-11-30 10:23:30 +0100

    if functie proberen werkende te krijgen

--- lib/boodschappen.php
+++ lib/boodschappen.php
@@ -27,20 +27,61 @@ class boodschappen {
     // ophalen boodschappen
     public function selectBoodschappen($gerecht_id) {
         $sql = "SELECT * FROM boodschappen WHERE gerecht_id = $gerecht_id";
+        $return = [];  
+
         $result = mysqli_query($this->connection, $sql);
-        $boodschappen = mysqli_fetch_array($result, MYSQLI_ASSOC);
-        return($boodschappen);
+        while ($boodschappen = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+            $boodschappenGerecht_id = $boodschappen["gerecht_id"];
+            $boodschappenUser_id = $boodschappen["user_id"];
+
+            //$ingredienten = 
+
+            $return[] =[ 
+                "gerecht_id_van_boodschappen" => $boodschappenGerecht_id,
+                "user_id_van_boodschappen" => $boodschappenUser_id
+            ];
+        }
+
         // dit willen we loopen om alles te zien 
+
+        return($return);
     }
 
-    public function selecteerartikel() {}
+    
+    public function boodschappenToevoegen($gerecht_id, $user_id) {
+        $sql = "SELECT * FROM boodschappen WHERE gerecht_id = $gerecht_id";
+
+        $result = mysqli_query($this->connection, $sql);
+        while ($boodschappen = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+            $boodschappenGerecht_id = $boodschappen["gerecht_id"];
+            $boodschappenUser_id = $boodschappen["user_id"];
+
+        if($boodschappenGerecht_id == $gerecht_id AND
+        $boodschappenUser_id == $user_id) {
+            echo "nice";
+            break;
+        } else { 
+            echo "bestaat nog niet";
+            /*
+            $sql = "INSERT INTO boodschappen (gerecht_id, user_id)
+            VALUES ($gerecht_id, $user_id)";
+            if ($this->connection->query($sql) == TRUE) {
+                echo "New record created successfully";
+              } else {
+                echo "Error: " . $sql . "<br>" . $this->connecction->error;
+              }
+              */
+        } // Einde else statement
+        } // einde while functie
+    
 
     // we moeten zoeken of de unieke combinatie van user en gerecht al in de boodschappenlijst staat. als dit niet het geval is voegen we die toe.
     // user id 
     //check if value is not in array
 
     // we moeten ook kijken wat we nodig hebben op de pagina zelf en deze info ophalen. Is hier de gerecht tabel voor nodig?
-}
+    } // Einde public function
+} // Einde class
 
 
 
