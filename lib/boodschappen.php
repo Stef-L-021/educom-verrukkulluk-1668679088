5d2ac8e38ece8c4a9944e90e7a5237fba099fbec
Stef Lipka <steflipka021@gmail.com>
2022-12-05 11:20:35 +0100

    Boodschappen verkort

--- lib/boodschappen.php
+++ lib/boodschappen.php
@@ -3,130 +3,98 @@
 class boodschappen {
     private $connection;
     private $ingredient;
-    private $user;
-    private $boodschappen;
-    public $aantal;
 
     public function __construct($connection) {
         $this->connection = $connection;
         $this->ingredient = new ingredient ($connection);
-        $this->user = new user($connection);
     }
 
-    //ophalen ingredienten voor het artikel_id te krijgen
-    private function selectIngredienten($gerecht_id){
+    // Ophalen van tabellen -------------------------------------------------------------------------------
+
+    // ophalen van ingredienten:
+    private function selectIngredienten($gerecht_id) {
         $ingredienten = $this->ingredient->selecteerIngredient($gerecht_id);
         return($ingredienten);
     }
 
-    // deze wordt volgens mij niet gebruikt.
-    private function selectUser($user_id) {
-        $data = $this->user->selecteerUser($user_id);
-        return($data);
-    }
-
-    // ophalen van de boodschappen tabel
+    // Ophalen van de boodschappenlijst tabel
     public function ophalenBoodschappen($user_id) {
-        $return = [];     
-        $sql = "select * from boodschappen where user_id = $user_id";
+        $return = [];
+        $sql = "select * FROM boodschappen where user_id = $user_id";
         $result = mysqli_query($this->connection, $sql);
         while ($boodschappen = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
             $return[] = $boodschappen;
         }
-        return($return);
+    return($return);
     }
 
+    // Aantal functie berekeningen -----------------------------------------------------------------------------
 
-    // functie voor het toevoegen van een nieuw artikel in de DB
-    private function toevoegenartikel($ingredient, $user_id) {
-        $artikel_id = $ingredient["artikel_id"];
-        
-        $aantalBerekening = $this->aantalBerekenen($ingredient);
-        $aantal = ceil($aantalBerekening);
-        $sql = "INSERT INTO boodschappen (artikel_id, user_id, aantal, precies_aantal) VALUES ($artikel_id, $user_id, $aantal, $aantalBerekening)";
-        $result = mysqli_query($this->connection, $sql); 
-        return TRUE;
-    }
-
-    // De aantal berekening voor nieuwe ingredienten en wordt gebruikt in de berekening voor de updates van ingredienten:
+    // aantal berekening die voor latere berekeningen worden gebruikt
     private function aantalBerekenen($ingredient) {
         $ingredientAantal = $ingredient["aantal"];
         $ingredientVerpakking = $ingredient["verpakking"];
         $aantalBerekening = $ingredientAantal/$ingredientVerpakking;
         return $aantalBerekening;
     }
-    
-    // De aantal berekening voor updates van ingredienten
+
+    // precies aantal berekenen
     private function preciesAantalUpdate($ingredient, $boodschap) {
-        $aantalBerekening = $this->aantalBerekenen($ingredient);
-        $berekeningPreciesAantal = $boodschap["precies_aantal"] + $aantalBerekening;
-        return ($berekeningPreciesAantal);
+        $aantalBerekening = $this->aantalBerekenen($ingredient);        // Ophalen van aantalberkenening van hierboven
+        $berekeningPreciesAantal = $boodschap["precies_aantal"] +$aantalBerekening;     // Hier tellen we de cell uit de DB van het precieze aantal bij de aantalberekening
+        return($berekeningPreciesAantal);
     }
 
-    // Functie voor het bijwerken van de aantallen van de artikelen in de DB.
-    private function artikelBijwerken($boodschap, $ingredient) {
-        $berekeningPreciesAantal = $this->preciesAantalUpdate($ingredient, $boodschap);
-        $aantal = ceil($berekeningPreciesAantal);
-        $sql = "UPDATE boodschappen SET aantal = $aantal, precies_aantal = $berekeningPreciesAantal
-        WHERE id = $boodschap[id]";
+    // functies maken die worden gebruikt in de final functie -------------------------------------------------------------
+
+    // check of het artikel al op de lijst staat:
+    private function artikelOpLijst($artikel_id, $user_id) {
+        $boodschappen = $this->ophalenBoodschappen($user_id);       // Ophalen van de boodschappen tabel
+        foreach ($boodschappen as $boodschap) {                     // Loop waar we door iedere rij van de tabel gaat genaamd $boodschap
+            if($boodschap["artikel_id"] == $artikel_id) {           // Als het artikel_id van de boodschappen tabel aan het $artikel_id wat we in de functie invullen (en die weer uit $ingredient komt)
+                return($boodschap);                                 // DAN returnen we die rij
+            }                                                       // Anders gebeurd er niks
+        }                                                           // blijf dat loopen totdat er geen nieuwe $boodschap rij is
+        return FALSE;                                               
+    }
+
+    // toevoegen van artikelen
+    private function toevoegenArtikel($ingredient, $user_id) {
+        $artikel_id = $ingredient["artikel_id"];                        // We halen hier artikel_id op omdat we deze in de tabel gaan invullen
+
+        $aantalBerekening = $this->aantalBerekenen($ingredient);        // Deze gaan we toevoegen in de kolom precies aantal
+        $aantal = ceil($aantalBerekening);                              // Dit wordt het afgeronde aantal dat wordt ingevuld in de DB voor de eerste keer
+        
+        $sql = "INSERT INTO boodschappen (artikel_id, user_id, aantal, precies_aantal)
+        VALUES($artikel_id, $user_id,  $aantal, $aantalBerekening)";
         $result = mysqli_query($this->connection, $sql);
     }
 
-    // functie om te checken of een artikel al op de lijst staat
-    private function artikelOpLijst($artikel_id, $user_id) {
-        $boodschappen = $this->ophalenBoodschappen($user_id);
-        foreach ($boodschappen as $boodschap) {
-            if($boodschap["artikel_id"] == $artikel_id) {
-                return($boodschap);
-            }
-        }
-        return FALSE; 
+    // artikel bijwerken
+    private function artikelBijwerken($boodschap, $ingredient) {
+        $berekeningPreciesAantal = $this->preciesAantalUpdate($ingredient, $boodschap);     // Hier halen we het precieze aantal op
+        $aantal = ceil($berekeningPreciesAantal);                       // Hier doen we dat ceilingen om het afgeronde aantal te krijgen
+
+        $sql = "UPDATE boodschappen SET aantal = $aantal, precies_aantal = $berekeningPreciesAantal
+        WHERE id = $boodschap[id]";                                     // aantal en precies aantal updaten in DB. 
+        $result = mysqli_query($this->connection, $sql);
     }
 
-    // Final functie waarbij oudere in voorkomen:
+    // Finale ----------------------------------------------------------------------------------------------------------------
+
+    // final functie:
     public function boodschappenToevoegen($gerecht_id, $user_id) {
-        $ingredienten = $this->selectIngredienten($gerecht_id);
-        foreach ($ingredienten as $ingredient) {
+        $ingredienten = $this->selectIngredienten($gerecht_id);     // Ophalen van ingredienten
+        foreach ($ingredienten as $ingredient) {                    // Loopen voor ieder ingredient
             $gevonden = $this->artikelOpLijst($ingredient["artikel_id"], $user_id);
-            if(!$gevonden) {    
-                $this->toevoegenartikel($ingredient, $user_id);
-            } else {
-                $this->artikelBijwerken($gevonden, $ingredient);
-            }
-        }            
-
-    
-
-    } // Einde public function
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      
-} // Einde class
-
-    
-
-    // we moeten zoeken of de unieke combinatie van user en gerecht al in de boodschappenlijst staat. als dit niet het geval is voegen we die toe.
-    // user id 
-    //check if value is not in array
-
-    // we moeten ook kijken wat we nodig hebben op de pagina zelf en deze info ophalen. Is hier de gerecht tabel voor nodig?
+        if(!$gevonden) {
+            $this->toevoegenartikel($ingredient, $user_id);
+        } else {
+            $this->artikelBijwerken($gevonden, $ingredient);
+        }
+        } // Einde foreach functie
+    } // Einde final public functie
+}
 
 
 
