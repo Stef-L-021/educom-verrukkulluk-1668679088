762036d4f7bb75707e1d88f14f9a7844eb83ae40
Stef Lipka <steflipka021@gmail.com>
2022-12-06 13:06:38 +0100

    Zoekfunctie verplaatst

--- lib/zoekfunctie.php
+++ lib/zoekfunctie.php
@@ -15,7 +15,7 @@ class zoekfunctie {
                 SELECT id from artikel WHERE naam LIKE '%$query%'
             )
         )";
-        echo $sql;
+        //echo $sql;
         $gevonden = [];
         $result = mysqli_query($this->connection, $sql);
         while($zoek = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
